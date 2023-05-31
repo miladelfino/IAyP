5b2befe570cae9901a4a2ebed6307c976c369b3c
miladelfino <miladelfino17@gmail.com>
2023-05-31 20:31:51 -0300

    Add files via upload

---
+++ Actividad1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+float altoPared;
+float anchoPared;
+float costoM2;
+float supPared;
+float costoMdeO;
+
+int main()
+{
+    printf("\n Ingrese el alto de la pared: ");
+    scanf("%f",&altoPared);
+    printf("\n Ingrese el ancho de la pared: ");
+    scanf("%f",&anchoPared);
+    printf("\n Ingrese el costo de la mano de obra: ");
+    scanf("%f",&costoM2);
+    supPared = anchoPared*altoPared;
+    costoMdeO= supPared*costoM2*1.2;
+    printf("El costo de Mano de Obra para pintar la pared es: %.2f\n",costoMdeO);
+    return 0;
+}
