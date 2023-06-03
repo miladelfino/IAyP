d5fa94788f7674f80dbc0fd529cc6845fcfa2572
miladelfino <miladelfino17@gmail.com>
2023-06-03 14:06:27 -0300

    Add files via upload

---
+++ Laboratorios/Laboratorio3.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+float a,b,c,d,e,f;
+float mayorNum;
+float Mayor(float x, float y);
+
+int main(){
+    
+    printf("ingrese 6 numeros: ");
+    scanf("%f" "%f" "%f" "%f" "%f" "%f", &a, &b, &c, &d, &e, &f);
+    mayorNum= Mayor(Mayor(Mayor(a,b), Mayor(c,d)), Mayor(e,f));
+    printf("%f", mayorNum);
+    
+    return 0;
+}
+
+
+float Mayor(float x, float y){
+    return (((x+y)+ fabs(x-y))/2);
+}
---
+++ Laboratorios/Laboratorio4.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+typedef struct {
+    float x;
+    float y;
+} Tpunto;
+
+typedef struct{
+    float a;
+    float b;
+} Trecta;
+
+Tpunto p1;
+Trecta rec;
+char msg [50];
+void cargaPunto(Tpunto *q);
+void cargaRecta(Trecta *coef);
+int pertenece(Tpunto p, Trecta recta);
+
+int main(){
+    cargaPunto(&p1);
+    cargaRecta(&rec);
+    if (pertenece(p1, rec)){
+        strcpy(msg, "El punto pertenece a la recta");
+    }else{
+        strcpy(msg, "El punto NO pertenece a la recta");
+    }
+    printf("%s", msg);
+    return 0;
+}
+
+void cargaPunto(Tpunto *p){
+    printf("Ingrese los puntos a de la recta (x;y): ");
+    scanf("%f %f", &p->x, &p->y);
+}
+
+void cargaRecta(Trecta *coef){
+    printf("Ingrese los coeficientes de a y b: ");
+    scanf("%f %f", &coef->a, &coef->b);
+}
+
+int pertenece(Tpunto p, Trecta recta){
+    return(p.y== p.x * recta.a + recta.b);
+}
+
