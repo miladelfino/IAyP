098cc48e8bcf193a5bcc107e415de565f2cf1e3f
miladelfino <miladelfino17@gmail.com>
2023-11-11 17:05:11 -0300

    Add files via upload

---
+++ Laboratorios/Laboratorio14a.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int num;
+int SumatoriaAumento(int n);
+
+int main(){
+    printf("Ingrese un  valor para ver la sumatoria desde 1 al numero dado, el numero debe ser natural: ");
+    scanf("%d", &num);
+    printf("Sumatoria con aumento: %d", SumatoriaAumento(num));
+}
+
+int SumatoriaAumento(int n){
+    if (n==1){
+        return 1;
+    }else{
+        return(n+SumatoriaAumento(n-1));
+    }
+}
